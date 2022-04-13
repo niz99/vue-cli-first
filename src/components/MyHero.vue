78927a58b047ae5a71ae81bfd526597a31a2efde
Nizar Fergag <fergag99@gmail.com>
2022-04-13 15:22:58 +0200

    hero inserita

---
+++ src/components/MyHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'MyHero'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div{
+    height: 450px;
+    background-image: url('../assets/img/norvegia.jpeg');
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+</style>
