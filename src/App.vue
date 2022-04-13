21f780041c2a97c941692d51f077b28994b23cd1
Nizar Fergag <fergag99@gmail.com>
2022-04-13 15:13:56 +0200

    titolo inserito

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyTitle/>
+  </div>
+</template>
+
+<script>
+import MyTitle from './components/MyTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyTitle
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1{
+    color: red;
+    font-size: 50px;
+}
+</style>
